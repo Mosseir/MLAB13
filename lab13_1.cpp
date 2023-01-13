1d68a871304115a20b6cbda963113793c52e331f
Mosseir <glock18_123@hotmail.com>
2023-01-13 14:04:34 +0700

    First Round Done

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+long long int fibonacci(long long int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long int fibonacci(long long int x){
+    if(x > 1){
+        return fibonacci(x-1) + fibonacci(x-2) ;
+    }else if(x == 0 || x == 1){
+        return x ;
+    }
+    return 0;
 }
